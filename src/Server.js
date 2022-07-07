32d8e2475ba35edfaa0d94a0fb3f8fd245b40ec9
Juan Pablo Cano <canojuan0415@gmail.com>
2022-07-07 16:42:32 -0500

    Feat: Se añade middleware para manejo de rutas no establecidas

--- src/Server.js
+++ src/Server.js
@@ -9,6 +9,12 @@ app.use(express.json());
 
 app.use('/api', AppRoutes);
 
+app.use((req, res) => {
+    res.status(404).json({
+        error: -2,
+        descripcion: `Ruta ${req.url} no implementada`
+    });
+})
 
 app.listen(Config.PORT, () => {
     console.log(`Servidor corriendo en el puerto ${Config.PORT}`);
